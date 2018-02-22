62acdc6e1809421706636655b267e54e5a43e241
Paul Sealock <psealock@gmail.com>
2018-02-22 13:02:16 +1300

    append text

--- client/extensions/woocommerce/components/d3/horizontal-bar/index.js
+++ client/extensions/woocommerce/components/d3/horizontal-bar/index.js
@@ -5,21 +5,24 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
-import { extent as d3Extent } from 'd3-array';
-import { scaleLinear as d3ScaleLinear } from 'd3-scale';
-import { line as d3Line } from 'd3-shape';
+// import PropTypes from 'prop-types';
+// import { extent as d3Extent } from 'd3-array';
+// import { scaleLinear as d3ScaleLinear } from 'd3-scale';
+// import { line as d3Line } from 'd3-shape';
 import classNames from 'classnames';
 import D3Base from 'woocommerce/components/d3/base';
 
 const HorizontalBar = ( { className, data } ) => {
 	const drawChart = svg => {
-		return svg.append( 'text' ).text( data );
+		return svg
+			.append( 'text' )
+			.attr( 'transform', 'translate(0,10)' )
+			.text( data );
 	};
 
 	const getParams = () => ( {
-		width: 20,
-		height: 20,
+		width: 100,
+		height: 10,
 	} );
 
 	return (
