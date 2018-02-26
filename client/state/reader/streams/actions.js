04a9e6da2e386cb503e7204652b4c81973b85903
Jake F - @samouri <samouri@users.noreply.github.com>
2018-02-26 12:17:07 -0500

    Redux streams

--- client/state/reader/streams/actions.js
+++ client/state/reader/streams/actions.js
@@ -8,8 +8,11 @@ import {
 	READER_STREAMS_PAGE_RECEIVE,
 	READER_STREAMS_SHOW_UPDATES,
 	READER_STREAMS_SELECT_ITEM,
+	READER_STREAMS_SELECT_NEXT_ITEM,
+	READER_STREAMS_SELECT_PREV_ITEM,
 	READER_STREAMS_FILL_GAP,
 	READER_STREAMS_DISMISS_POST,
+	READER_STREAMS_UPDATES_RECEIVE,
 } from 'state/action-types';
 
 /**
@@ -18,69 +21,79 @@ import {
  * This action will fetch a range of posts for a stream and then dispatch
  * READER_STREAM_PAGE_RECEIVE when the page returns. This is usually used to
  * fetch the next page of results, but could be used to fetch arbitrary ranges.
- * @param  {string} streamId The stream to fetch posts for
+ * @param  {string} streamKey The stream to fetch posts for
  * @param  {object} query    The query for posts. Parameters vary by stream type.
  * @return {object}          The action object
  */
-export function requestPage( { streamId, query } ) {
-	const indexOfColon = streamId.indexOf( ':' );
-	const streamType = indexOfColon === -1 ? streamId : streamId.substring( 0, indexOfColon );
+export function requestPage( { streamKey, query } ) {
+	const indexOfColon = streamKey.indexOf( ':' );
+	const streamType = indexOfColon === -1 ? streamKey : streamKey.substring( 0, indexOfColon );
 	return {
 		type: READER_STREAMS_PAGE_REQUEST,
 		payload: {
-			streamId,
+			streamKey,
 			query,
 			streamType,
 		},
 	};
 }
 
-export function receivePage( { streamId, query, posts } ) {
+export function receivePage( { streamKey, query, posts } ) {
 	return {
 		type: READER_STREAMS_PAGE_RECEIVE,
 		payload: {
-			streamId,
+			streamKey,
 			query,
 			posts,
 		},
 	};
 }
 
-export function showUpdates( { streamId } ) {
+export function showUpdates( { streamKey } ) {
 	return {
 		type: READER_STREAMS_SHOW_UPDATES,
-		payload: {
-			streamId,
-		},
+		payload: { streamKey },
 	};
 }
 
-export function selectItem( { streamId, index } ) {
+export function receiveUpdates( { streamKey, posts } ) {
+	return {
+		type: READER_STREAMS_UPDATES_RECEIVE,
+		payload: { streamKey, posts },
+	};
+}
+
+export function selectItem( { streamKey } ) {
 	return {
 		type: READER_STREAMS_SELECT_ITEM,
-		payload: {
-			streamId,
-			index,
-		},
+		payload: { streamKey },
 	};
 }
 
-export function fillGap( { streamId, gap } ) {
+export function selectNextItem( { streamKey } ) {
+	return {
+		type: READER_STREAMS_SELECT_NEXT_ITEM,
+		payload: { streamKey },
+	};
+}
+
+export function selectPrevItem( { streamKey } ) {
+	return {
+		type: READER_STREAMS_SELECT_PREV_ITEM,
+		payload: { streamKey },
+	};
+}
+
+export function fillGap( { streamKey, gap } ) {
 	return {
 		type: READER_STREAMS_FILL_GAP,
-		payload: {
-			streamId,
-			gap,
-		},
+		payload: { streamKey, gap },
 	};
 }
 
-export function dismissPost( { streamId, postId } ) {
+export function dismissPost( { streamKey, postId } ) {
 	return {
 		type: READER_STREAMS_DISMISS_POST,
-		payload: {
-			streamId,
-			postId,
-		},
+		payload: { streamKey, postId },
 	};
 }
--- client/state/reader/streams/reducer.js
+++ client/state/reader/streams/reducer.js
@@ -1,13 +1,53 @@
 /** @format */
 
 /**
+ * External dependencies
+ */
+import { uniqBy } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { keyedReducer, combineReducers } from 'state/utils';
-import { itemsSchema } from './schema';
-import { READER_STREAMS_PAGE_RECEIVE, READER_STREAMS_SELECT_ITEM } from 'state/action-types';
+import {
+	READER_STREAMS_PAGE_RECEIVE,
+	READER_STREAMS_SELECT_ITEM,
+	READER_STREAMS_UPDATES_RECEIVE,
+} from 'state/action-types';
+import { keyToString } from './post-key';
 
-export const items = keyedReducer( 'payload.streamId', ( state = [], action ) => {
+export const items = ( state = [], action ) => {
+	if ( action.payload.error ) {
+	}
+
+	this.oldestPostDate = get( data, [ 'date_range', 'after' ] );
+	this.lastPageHandle = get( data, [ 'meta', 'next_page' ], null );
+
+	const posts = data && data.posts;
+
+	if ( ! posts ) {
+		this.emitChange();
+		return;
+	}
+
+	if ( ! posts.length ) {
+		this._isLastPage = true;
+		this.emitChange();
+		return;
+	}
+
+	const postKeys = this.filterNewPosts( posts );
+
+	if ( postKeys.length ) {
+		const postById = this.postById;
+		forEach( postKeys, function( postKey ) {
+			postById.add( keyToString( postKey ) );
+		} );
+		this.postKeys = this.postKeys.concat( postKeys );
+	}
+
+	this.page++;
+	this.emitChange();
 	switch ( action.type ) {
 		case READER_STREAMS_PAGE_RECEIVE:
 			const { posts } = action.payload;
@@ -26,20 +66,31 @@ export const items = keyedReducer( 'payload.streamId', ( state = [], action ) =>
 		default:
 			return state;
 	}
-} );
+};
 
-items.schema = itemsSchema;
-
-export const selected = keyedReducer( 'payload.streamId', ( state = [], action ) => {
+export const pendingItems = ( state = [], action ) => {
 	switch ( action.type ) {
-		case READER_STREAMS_SELECT_ITEM:
+		case READER_STREAMS_UPDATES_RECEIVE:
+			const { postKeys } = action.payload;
+			return uniqBy( [ ...postKeys, ...state ], keyToString );
+		default:
+			return state;
+	}
+};
+
+export const selected = ( state = null, action ) => {
+	switch ( action.type ) {
+		case READER_STREAMS_SELECT_ITEM: // probably wants to actually open post instead of select?
 			return action.payload.index;
 		default:
 			return state;
 	}
-} );
+};
 
-export default combineReducers( {
+const streamReducer = combineReducers( {
 	items,
+	pendingItems,
 	selected,
 } );
+
+export default keyedReducer( 'payload.streamKey', streamReducer );
